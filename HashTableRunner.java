a9d2670ea4377a2127273e9ff68eef87ac9a99ac
JailynPerales <132614537+JailynPerales@users.noreply.github.com>
2025-02-28 20:34:10 -0600

    Update HashTableRunner.java

--- HashTableRunner.java
+++ HashTableRunner.java
@@ -1,12 +1,12 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
+//Name - Jailyn Perales
 
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 import java.io.File;
 import static java.lang.System.*;
+import java.util.Hashtable;
 
 public class HashTableRunner
 {
@@ -14,13 +14,37 @@ public class HashTableRunner
   {
 		try{
 			//make a new table			
-			
+			Hashtable<Integer, String> hash = new Hashtable<Integer, String>();
 			//read from the file			
-			
+			Scanner file = new Scanner(new File("Numbers.dat"));
 			//load stuff into the table			
-			
-			//print out the table
+			while (file.hasNextInt()) {
+			    Number num = new Number(file.nextInt());
+			    if(hash.containsKey(num.hashCode())){
+			        boolean another = false;
+			        String[] list = hash.get(num.hashCode()).split(" ");
+			        for(int i = 0; i < list.length; i++){
+			            if(list[i].equals(""+ num)){
+			                another = true;
+			                break;
+			            }
+			        }
+			        if(!another){
+			            hash.put(num.hashCode(), hash.get(num.hashCode()) + " " + num);
+			        }
+			    }
+			    else{
+			        hash.put(num.hashCode(), "" + num);
+			    }
+			}
+			Object[] keySet = hash.keySet().toArray();
+			System.out.println("HASHTABLE");
+			for(int i = (hash.size()-1); i >= 0; i--) {
+			    System.out.println("bucket " + keySet[i] + " " + hash.get(keySet[i]));
+			}
+
 		}
+		
 		catch(Exception e)
 		{
 			System.out.println("Houston, we have a problem!");
